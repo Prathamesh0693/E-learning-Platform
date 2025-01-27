f16a8969fdd1830f1b33c5254908a8e94c51dcfd
Prathamesh Patil <prathameshpatil0693@gmail.com>
2025-01-27 21:14:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let btn1 = document.querySelector("#buyBtn1");
+let btn2 = document.querySelector("#buyBtn2");
+let btn3 = document.querySelector("#buyBtn3");
+let btn4 = document.querySelector("#buyBtn4");
+let btn5 = document.querySelector("#buyBtn5");
+let btn6 = document.querySelector("#buyBtn6");
+let btn7 = document.querySelector("#buyBtn7");
+let btn8 = document.querySelector("#buyBtn8");
+btn1.addEventListener("click", () => {
+    let url = "https://www.youtube.com/watch?v=BsDoLVMnmZs&t=11s&pp=ygUYaHRtbCBjb21wbGV0ZSBjb3Vyc2UgQ1dI";
+    window.open(url, "_blank")
+})
+btn2.addEventListener("click", () => {
+    let url = "https://www.youtube.com/watch?v=EUtlj7xdO1o&pp=ygUXY3NzIG9uZSBzaG90IGFudXJhZyBzaXI%3D";
+    window.open(url, "_blank")
+})
+btn3.addEventListener("click", () => {
+    let url = "https://www.youtube.com/watch?v=UmnCZ7-9yDY&pp=ygUZamF2YSBieSBhcG5hIGNsZyBvbmUgc2hvdA%3D%3D";
+    window.open(url, "_blank")
+})
+btn4.addEventListener("click", () => {
+    let url = "https://www.youtube.com/watch?v=NNLoi8QqzaY&pp=ygUQamF2YSBieSBhbnVqIHNpcg%3D%3D";
+    window.open(url, "_blank")
+})
+btn5.addEventListener("click", () => {
+    let url = "https://www.youtube.com/watch?v=UrsmFxEIp5k&t=14297s&pp=ygUNcHl0aG9uIGJ5IGN3aA%3D%3D";
+    window.open(url, "_blank")
+})
+btn6.addEventListener("click", () => {
+    let url = "https://www.youtube.com/watch?v=bL-o2xBENY0&list=PLxgZQoSe9cg0df_GxVjz3DD_Gck5tMXAd";
+    window.open(url, "_blank")
+})
+btn7.addEventListener("click", () => {
+    let url = "https://www.youtube.com/watch?v=q_JsgpiuY98&list=PL9ooVrP1hQOG6DQnOD6ujdCEchaqADfCU";
+    window.open(url, "_blank")
+})
+btn8.addEventListener("click", () => {
+    let url = "https://www.youtube.com/watch?v=4EjKroJCpFA&list=PLA3GkZPtsafZydhN4nP0h7hw7PQuLsBv1";
+    window.open(url, "_blank")
+})
+
